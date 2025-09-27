698ad123a67432c55891efb8ff8d50269ec3e1cc
Tuan Kiet <trankietpm@gmail.com>
2025-09-27 14:25:04 +0700

    Initial commit

---
+++ src/modules/Homepage/Homepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Homepage.module.scss'
+
+function Homepage() {
+  return (
+    <div className={styles.homepage}>Homepage</div>
+  )
+}
+
+export default Homepage
