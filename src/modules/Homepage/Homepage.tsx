d2a63d73f9bedbb2c8736bbaf2d1aee9d6d9fe2b
MKoiii <leminhkhoi101@gmail.com>
2025-09-28 22:07:46 +0700

    add homepage and sign in/up

--- src/modules/Homepage/Homepage.tsx
+++ src/modules/Homepage/Homepage.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import styles from './Homepage.module.scss'
+import Hero from '@/components/Hero/Hero'
+import Welcome from '@/components/Welcome/Welcome'
 
 function Homepage() {
   return (
-    <div className={styles.homepage}>Homepage</div>
+    <div className={styles.homepage}>
+      <div className={styles.hero}>
+        <Hero />
+      </div>
+      <div className={styles.welcome}>
+        <Welcome />
+      </div>
+    </div>
   )
 }
 
